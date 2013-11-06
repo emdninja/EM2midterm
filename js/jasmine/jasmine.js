a8d8e6de9d5efe068b5db89c217c64fe5db9cf6a
jasmine <jasminezhou27@gmail.com>
2013-11-06 13:23:28 -0800

    final changes to midterm

--- js/jasmine/jasmine.js
+++ js/jasmine/jasmine.js
@@ -2,17 +2,17 @@ jQuery(document).ready(function($) {
  	var container= $("#jasmine");
 	var title = $("#jasmine .inner .title");
 	var bg = $("#jasmine .inner .bg");
-	var q = $("#jasmine .inner .quags");
+	var art = $("#jasmine .inner .art");
 	var bgColour = "#dbb019";
 
 	resize();
 
 	$('#jasmine').mouseenter(function(event) {
 		console.log("mouse has entered");
-		TweenMax.to(container, .25,{css:{backgroundColor:bgColour}});
+		TweenMax.to(container, .25);
 		TweenMax.to(title, .5, {css:{top:-200 }, ease:Power1.easeInOut});
 		TweenMax.to(bg, .2, {css:{top:0 }, ease:Power1.easeInOut});
-		TweenMax.to(q, .2, {css:{top:0 }, ease:Power1.easeInOut, delay:.5});
+		TweenMax.to(art, .2, {css:{top:0 }, ease:Power1.easeInOut, delay:.5});
 	});	
 
 	$('#jasmine').mouseleave(function(event) {
@@ -20,12 +20,12 @@ jQuery(document).ready(function($) {
 		TweenMax.to(title, .75, {css:{top:0 }, ease:Bounce.easeInOut,delay:.25,
 
 			onComplete:function(){
-				TweenMax.to(container, .25,{css:{backgroundColor:"#000000"}});
+				TweenMax.to(container, .25);
 				TweenMax.to(bg, .25, {css:{top:-480 }, ease:Power1.easeInOut});
 			}
 		});
 
-		TweenMax.to(q, .5, {css:{top:480 }, ease:Power1.easeInOut});
+		TweenMax.to(art, .5, {css:{top:480 }, ease:Power1.easeInOut});
 });
 
 $(window).resize(function(){	
@@ -36,7 +36,7 @@ function resize(){
 	var w = $(container).width();
 	$(title).width(w);
 	$(bg).width(w);
-	$(q).width(w);
+	$(art).width(w);
 
 	$(container).css({'min-height':w});
 }
